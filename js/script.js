2bea05f4737b5325fa4632adab3ccbdedcc962f9
r9rizwan <81493506+r9rizwan@users.noreply.github.com>
2022-03-30 02:12:04 +0100

    updated version

--- js/script.js
+++ js/script.js
@@ -2,24 +2,41 @@ function table(event){
 
     event.preventDefault();
 
+    
+
+    //get div id for displaying table
     var table = document.getElementById("t1");
-    var newTable = document.createElement("TABLE");
-    newTable.setAttribute("id", "myTable");
+    var rows = document.getElementById("rows").value;
+    var columns = document.getElementById("columns").value;
 
-    table.appendChild(newTable);
+    //create table and table body
+    var tbl = document.createElement("table");
+    var tblBody = document.createElement("tbody")
+    
 
-    var y = document.createElement("TR");
-    var y1 = y.createTextNode("Name", "Email", "URl");
-    y1.setAttribute("id", "myTr");
-    document.table.appendChild(y1);
+    for (var i = 0; i < rows; i++) {
+        // creates a table row
+        var row = document.createElement("tr");
+    
+        for (var j = 0; j < columns; j++) {
+          // Create a <td> element and a text node, make the text
+          // node the contents of the <td>, and put the <td> at
+          // the end of the table row
+          var cell = document.createElement("td");
+          var cellText = document.createTextNode("Row = "+i+", Column = "+j);
+          cell.appendChild(cellText);
+          row.appendChild(cell);
+        }
+
+    // add the row to the end of the table body
+    tblBody.appendChild(row);
+}
 
 
-
-
-    var z = document.createElement("TD");
-    var t = document.createTextNode("cell");
-    z.appendChild(t);
-    document.getElementById("myTr").appendChild(z);
-
-
+  // put the <tbody> in the <table>
+  tbl.appendChild(tblBody);
+  // appends <table> into <body>
+    table.appendChild(tbl);
+  // sets the border attribute of tbl to 2;
+  tbl.setAttribute("border", "2");
 }
